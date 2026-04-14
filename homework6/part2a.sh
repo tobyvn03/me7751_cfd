d6bce3fdfc2a7b749a96442158a01b913bf8a259
Toby Viet Nguyen <tobyn2003@gmail.com>
2026-04-13 20:05:00 -0400

    still cant figure it out

--- homework6/part2a.sh
+++ homework6/part2a.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python solver.py -i input_part2a.csv -o output_part2a.csv -f part2a -p plot_part2a -v -s -m --plot_every 100
+python solver.py -i input_part2a.csv -o output_part2a.csv -f part2a -p plot_part2a -v --plot_every 100
